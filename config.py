5fefc80ec4a7ac3208789146eec49706c4d9fd88
ElonMusk49 <101656030+ElonMusk49@users.noreply.github.com>
2022-06-08 21:52:30 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "5517902699:AAFTfPo-vi9eF2sPSyDd-APkBqzxfagL_eg"
+  APP_ID = 13780698
+  API_HASH = "56536f4127ca90a7785d11d2c8e1f031"
+  AUTH_CHANNEL = -1001234567890
